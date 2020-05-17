a4425c5810f8ef6de98b52601b3b415b3875551b
siddhary87 <siddhary87@gmail.com>
2020-05-17 22:42:19 +0530

    fix

---
+++ connect-auth/src/main/java/security/AuthenticateREST.java
@@ -0,0 +1,35 @@
+package security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class AuthenticateREST {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private PBKDF2Encoder passwordEncoder;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Mono<ResponseEntity<?>> login(@RequestBody AuthRequest ar) {
+        return userService.findByUsername(ar.getUsername()).map((userDetails) -> {
+            if (passwordEncoder.encode(ar.getPassword()).equals(userDetails.getPassword())) {
+                return ResponseEntity.ok(new AuthResponse(jwtUtil.generateToken(userDetails)));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        }).defaultIfEmpty(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+
+}
