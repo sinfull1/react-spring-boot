76f33e1da457768bf6d55e55e10a06ddc62cef5e
siddhary <sid.infinity.yadav@gmail.com>
2020-10-19 23:13:48 +0530

    1

--- connect-server/src/main/java/connect/controllers/HttpBinController.java
+++ connect-server/src/main/java/connect/controllers/HttpBinController.java
@@ -1,25 +1,14 @@
 package connect.controllers;
 
-import connect.processor.EventProcessor;
-import org.reactivestreams.Subscriber;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
-
-import java.time.Duration;
 
 
 @CrossOrigin(origins = {"*"})
 @RestController
 @CacheConfig(cacheNames = {"httpbin"})
 public class HttpBinController {
-
-
 
 
 }
