3fa5a518a749bef23ffd00d62c6c4cf1773b9d41
siddhary <sid.infinity.yadav@gmail.com>
2021-01-07 23:52:03 +0530

    prebuild push

---
+++ connect-react/build/precache-manifest.07641fd61dc6500e8827d180db95f926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dee645504053b625b876ba9b1d7602d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ad73c8650e272404c5",
+    "url": "/static/css/2.9c6b7abf.chunk.css"
+  },
+  {
+    "revision": "c25c5f43c1d544764254",
+    "url": "/static/css/main.17540575.chunk.css"
+  },
+  {
+    "revision": "54ad73c8650e272404c5",
+    "url": "/static/js/2.c7bd4a38.chunk.js"
+  },
+  {
+    "revision": "5588c0dac69fea3ec31317c57c768601",
+    "url": "/static/js/2.c7bd4a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25c5f43c1d544764254",
+    "url": "/static/js/main.c479ebba.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  }
+]);
