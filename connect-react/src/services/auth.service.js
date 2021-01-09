51a726a5e12a232fa3f39637d409a91de91f8092
siddhary <sid.infinity.yadav@gmail.com>
2021-01-09 19:22:53 +0530

    fix

--- connect-react/src/services/auth.service.js
+++ connect-react/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://localhost:8443/";
+const API_URL = "https://www.gopaychain.in/";
 
 class AuthService {
   login(username, password) {
--- connect-react/src/upload/upload-files.service.js
+++ connect-react/src/upload/upload-files.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://localhost:8443",
+  baseURL: "https://www.gopaychain.in",
 });
 
 class UploadFilesService {
