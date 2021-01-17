15dd93a02c938f9d17cd5927896b925f8fc059cf
siddhary <sid.infinity.yadav@gmail.com>
2021-01-17 23:01:51 +0530

    remove h

---
+++ connect-web/precache-manifest.369f7e4cab9eb6dc274de4f8d8cf5b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371cfc3023b25b2d5e56b77124f317c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19f8519fba7027992cf3",
+    "url": "/static/css/2.9c6b7abf.chunk.css"
+  },
+  {
+    "revision": "6d4703a45943f4086cd1",
+    "url": "/static/css/main.21c48301.chunk.css"
+  },
+  {
+    "revision": "19f8519fba7027992cf3",
+    "url": "/static/js/2.75809ad0.chunk.js"
+  },
+  {
+    "revision": "62a1739c96b0628e61d23906cf07f1ce",
+    "url": "/static/js/2.75809ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4703a45943f4086cd1",
+    "url": "/static/js/main.5fb545e0.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  }
+]);
---
+++ connect-web/webpack.dev.js
@@ -0,0 +1,53 @@
+ const path = require('path');
+ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    output: {
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+                 test: /\.css$/i,
+                 use: [
+                           MiniCssExtractPlugin.loader,
+                           'css-loader',
+                 ],
+               },
+    ],
+  },
+
+  plugins: [
+         new MiniCssExtractPlugin({
+           filename: 'css/[name].css'
+         }),
+             new HtmlWebpackPlugin({
+               template: "./public/index.html",
+               filename: "./index.html",
+             }),
+
+
+       ],
+
+devServer: {
+  historyApiFallback: true
+  }
+
+};
