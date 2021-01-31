dcfcc89fc3ba3b1017555a395fff2375f58f5042
siddhary <sid.infinity.yadav@gmail.com>
2021-02-01 02:26:02 +0530

    remove  post

---
+++ connect-react/build/precache-manifest.4f2a36f600cf97cac3786815400e4a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48b29ad7082a37a19b14624c77e27b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263da2b3d4e85f807832",
+    "url": "/static/css/2.c3053dfc.chunk.css"
+  },
+  {
+    "revision": "a2421193b48b22a91f50",
+    "url": "/static/css/main.53d0d31e.chunk.css"
+  },
+  {
+    "revision": "263da2b3d4e85f807832",
+    "url": "/static/js/2.543feedb.chunk.js"
+  },
+  {
+    "revision": "676b4b0aad4015f452458d83dc5a4509",
+    "url": "/static/js/2.543feedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2421193b48b22a91f50",
+    "url": "/static/js/main.91b4d6ed.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  }
+]);
