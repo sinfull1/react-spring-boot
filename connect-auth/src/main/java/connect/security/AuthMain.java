23c4bac897e55ef955cd8a6a161a43d45db30e64
siddhary <sid.infinity.yadav@gmail.com>
2021-02-02 14:15:53 +0530

    fix

--- connect-auth/src/main/java/connect/security/AuthMain.java
+++ connect-auth/src/main/java/connect/security/AuthMain.java
@@ -1,14 +1,25 @@
 package connect.security;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import java.net.URI;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 @SpringBootApplication
 
 
@@ -28,5 +39,13 @@ public class AuthMain {
                     .allowedMethods("PUT,GET")
                     .maxAge(3600);
         }
+    }
+
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return  route(GET("/login"), req ->
+                ServerResponse.temporaryRedirect(URI.create("/home"))
+                        .build());
     }
 }
